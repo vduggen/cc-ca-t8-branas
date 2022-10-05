e17c2706b122090151d1c29f19e493c23fb36bea
Vitor Duggen <zduggem@gmail.com>
2022-10-05 19:41:11 -0300

    Criado teste para o checkout

--- src/tests/Checkout.test.ts
+++ src/tests/Checkout.test.ts
@@ -1,12 +1,30 @@
-import axios from "axios"
+import Checkout from "../application/Checkout";
+
+import Dimension from "../domain/entity/Dimension";
+import Item from "../domain/entity/Item";
+
+import OrderRepositoryMemory from "../infra/repository/memory/OrderRepositoryMemory";
+import ItemRepositoryMemory from "../infra/repository/memory/ItemRepositoryMemory";
+import GetOrderByCpf from "../application/GetOrderByCpf";
 
 describe('Checkout', function() {
-    test('Deve fazer um pedido', function() {
-        axios.post('http://localhost:3000/checkout', {
-            cpf: '092.216.699-470',
+    test('Deve fazer um pedido', async function() {
+        const itemRepository = new ItemRepositoryMemory();
+        await itemRepository.save(new Item(1, 'Camera', 1000, new Dimension(20, 15, 10), 0.003));
+
+        const orderRepository = new OrderRepositoryMemory();
+
+        const checkout = new Checkout(itemRepository, orderRepository);
+        const inputCheckout = {
+            cpf: '092.216.699-47',
             orderItems: [
-                { idItem: 1, quantity: 3 }
+                { idItem: 1, quantity: 1 }
             ]
-        })
+        }
+        await checkout.execute(inputCheckout);
+
+        const getOrderByCpf = new GetOrderByCpf(orderRepository);
+        const ordersByCpf = await getOrderByCpf.execute(inputCheckout.cpf);
+        expect(ordersByCpf).toHaveLength(1);
     })
 })

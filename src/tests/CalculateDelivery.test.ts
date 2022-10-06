33f93244bbf62c1f0271296722241c6685db83f6
Vitor Duggen <zduggem@gmail.com>
2022-10-06 17:39:28 -0300

    Adicionado mais testes no CalculateDelivery

--- src/tests/CalculateDelivery.test.ts
+++ src/tests/CalculateDelivery.test.ts
@@ -4,17 +4,27 @@ import Item from "@domain/entity/Item";
 import ItemRepositoryMemory from "@infra/repository/memory/ItemRepositoryMemory";
 import CalculateDelivery, { InputCalculateDelivery } from "../application/CalculateDelivery";
 
-describe('CalculateDelivery', function() {
-    test('Deve simular o frete', async function() {
-        const itemRepository = new ItemRepositoryMemory();
-        itemRepository.save(new Item(1, 'Camera', 1000, new Dimension(20, 15, 10), 0.003));
+const makeSut = async (item: Item) => {
+    const itemRepository = new ItemRepositoryMemory();
+    itemRepository.save(item);
 
-        const delivery = new Delivery();
-        const calculateDelivery = new CalculateDelivery(delivery, itemRepository);
-        const input: InputCalculateDelivery = {
-            idItem: 1,
-            distance: 1000
-        };
-        calculateDelivery.execute(input)
+    const delivery = new Delivery();
+    const calculateDelivery = new CalculateDelivery(delivery, itemRepository);
+    const input: InputCalculateDelivery = {
+        idItem: 1,
+        distance: 1000
+    };
+    return await calculateDelivery.execute(input);
+}
+
+describe('CalculateDelivery', function() {
+    test('Deve simular o frete (camera)', async function() {
+        const result = makeSut(new Item(1, 'Camera', 1000, new Dimension(20, 15, 10), 1));
+        expect(result).toBe(10);
+    })
+
+    test('Deve simular o frete (guitarra)', async function() {
+        const result = makeSut(new Item(1, 'Guitarra', 1000, new Dimension(100, 30, 10), 3));
+        expect(result).toBe(30);
     })
 })

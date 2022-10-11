56f705fbd0931adcfceaa745b2d97d5dcac6027b
Vitor Duggen <zduggem@gmail.com>
2022-10-11 12:25:21 -0300

    Refatorado o CalculateDelivery

--- src/tests/CalculateDelivery.test.ts
+++ src/tests/CalculateDelivery.test.ts
@@ -1,30 +1,19 @@
-import Delivery from "@domain/entity/Delivery"
+import Delivery from "@domain/entity/Delivery";
 import Dimension from "@domain/entity/Dimension";
 import Item from "@domain/entity/Item";
-import ItemRepositoryMemory from "@infra/repository/memory/ItemRepositoryMemory";
-import CalculateDelivery, { InputCalculateDelivery } from "../application/CalculateDelivery";
-
-const makeSut = async (item: Item) => {
-    const itemRepository = new ItemRepositoryMemory();
-    itemRepository.save(item);
-
-    const delivery = new Delivery();
-    const calculateDelivery = new CalculateDelivery(delivery, itemRepository);
-    const input: InputCalculateDelivery = {
-        idItem: 1,
-        distance: 1000
-    };
-    return await calculateDelivery.execute(input);
-}
 
 describe('CalculateDelivery', function() {
-    test('Deve simular o frete (camera)', async function() {
-        const result = await makeSut(new Item(1, 'Camera', 1000, new Dimension(20, 15, 10), 1));
-        expect(result).toBe(10);
+    test('Deve simular o frete com preço mínimo', async function() {
+        const delivery = new Delivery();
+        const item = new Item(1, 'Camera', 1000, new Dimension(20, 15, 10), 1);
+        const result = delivery.calculate(1000, item.size, item.density);
+        expect(result).toBe(10)
     })
 
-    test('Deve simular o frete (guitarra)', async function() {
-        const result = await makeSut(new Item(1, 'Guitarra', 1000, new Dimension(100, 30, 10), 3));
-        expect(result).toBe(30);
+    test('Deve simular o frete', async function() {
+        const delivery = new Delivery();
+        const item = new Item(1, 'Guitarra', 1000, new Dimension(100, 30, 10), 3);
+        const result = delivery.calculate(1000, item.size, item.density);
+        expect(result).toBe(30)
     })
 })

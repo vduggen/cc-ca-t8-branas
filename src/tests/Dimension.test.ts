f040000fa3d82e63233f31626dc91cfa28000ec6
Vitor Duggen <zduggem@gmail.com>
2022-10-11 13:48:32 -0300

    Adicionado testes no Dimension

--- src/tests/Dimension.test.ts
+++ src/tests/Dimension.test.ts
@@ -1,8 +1,23 @@
 import Dimension from "@domain/entity/Dimension";
 
 describe('Dimension', function() {
-    test('As dimensões não podem ser negativas', function() {
+    test('A largura não pode ser negativa', function() {
         const dimension = () => new Dimension(-20, 0, 0, 0);
+        expect(dimension).toThrow('Invalid dimension');
+    })
+
+    test('A altura não pode ser negativa', function() {
+        const dimension = () => new Dimension(0, -20, 0, 0);
+        expect(dimension).toThrow('Invalid dimension');
+    })
+    
+    test('A profundidade não pode ser negativa', function() {
+        const dimension = () => new Dimension(0, 0, -20, 0);
+        expect(dimension).toThrow('Invalid dimension');
+    })
+    
+    test('O peso não pode ser negativo', function() {
+        const dimension = () => new Dimension(0, 0, 0, -20);
         expect(dimension).toThrow('Invalid dimension');
     })
 })
